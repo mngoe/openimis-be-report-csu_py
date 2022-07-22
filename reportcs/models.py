beed359efa284ac8350f64a5b48ad26bf6c27b26
FrancineMADOH <francinemadoh@gmail.com>
2022-07-22 12:45:10 +0100

    .

--- reportcs/models.py
+++ reportcs/models.py
@@ -4,30 +4,5 @@ from core import models as core_models
 
 # Create your models here.
 class TestReport(core_models.UUIDModel):
-    """ Model test"""
-    REPORT_BRO = 0
-    REPORT_ENGINE_CHOICES = (
-        (REPORT_BRO, "Report Bro - PDF"),
-    )
-    #name = models.CharField(max_length=255, blank=False, null=False, unique=True)
-    #engine = models.IntegerField( choices=REPORT_ENGINE_CHOICES, default=REPORT_BRO)
-    RoleId =  models.IntegerField(null=False)
-    RoleName = models.CharField(max_length=50)
-
-    class Meta:
-        managed = True
-        db_table = "tblRole"
-
-#fonction 
-
-# def test_portdata_query(user,date_from=None, date_to=None, **kwargs):
-#     if date_from:
-#         filters &= Q(validity_from__gte=date_from)
-
-#     queryset = (
-#         Insuree.objects.filter(filters).values(
-#             "","","",enroll_date=F(""),
-#         )
-#     )
-
-#     return {"data": list(queryset)}
+    """ Model test """
+    
--- reportcs/report.py
+++ reportcs/report.py
@@ -1,14 +1,3 @@
 
-# report_definitions = [
-#     {
-#         "name": "insuree_family_overview",
-#         "engine": 0,
-#         "default_report": insuree_family_overview.template,
-#         "description": "Simple claim report",
-#         "module": "insuree",
-#         "python_query": insuree_familyoverview_query,
-#         "permission": [""],
-#     },
 
-# ]
 
