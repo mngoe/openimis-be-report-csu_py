b07f988787396c31801360e05b452639169c4575
FrancineMADOH <francinemadoh@gmail.com>
2022-07-22 09:52:48 +0100

    add the initial report model

--- reportcs/models.py
+++ reportcs/models.py
@@ -1,3 +1,33 @@
+from uuid import UUID
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+class TestReport(core_models.UUIDModel):
+    """ Model test"""
+    REPORT_BRO = 0
+    REPORT_ENGINE_CHOICES = (
+        (REPORT_BRO, "Report Bro - PDF"),
+    )
+    name = models.CharField(max_length=255, blank=False, null=False, unique=True)
+    engine = models.IntegerField( choices=REPORT_ENGINE_CHOICES, default=REPORT_BRO)
+    definition =  models.TextField()
+    title = models.TextField()
+
+    class Meta:
+        managed = True
+        db_table = "report_TestReport"
+
+#fonction 
+
+# def test_portdata_query(user,date_from=None, date_to=None, **kwargs):
+#     if date_from:
+#         filters &= Q(validity_from__gte=date_from)
+
+#     queryset = (
+#         Insuree.objects.filter(filters).values(
+#             "","","",enroll_date=F(""),
+#         )
+#     )
+
+#     return {"data": list(queryset)}
---
+++ reportcs/test_report.py
@@ -0,0 +1,25 @@
+test_report = """ \
+    {
+        "elementType": "text",
+    },{
+      "elementType": "text",  
+    },
+    {
+      "elementType": "text",  
+    },
+    {
+      "elementType": "text",  
+    },
+    {
+      "id": 3,
+      "name": "report_name",
+      "type": "string",
+      "arrayItemType": "string",
+      "eval": false,
+      "nullable": false,
+      "pattern": "",
+      "expression": "",
+      "showOnlyNameType": false,
+      "testData": ""
+    },
+"""
