be12835a4d57c2cf6bfe10696019eac0fba7505f
HERVEBORISMBOULONTSI <mherveboris@gmail.com>
2022-11-30 13:18:20 +0100

    facturer payé

--- reportcs/models.py
+++ reportcs/models.py
@@ -451,11 +451,12 @@ def periodic_rejected_bills_query(user, **kwargs):
             validity_to__isnull=True
             ).first()
         dictBase["fosa"] = hflocationObj.name
+        
         dictGeo['health_facility'] = hflocationObj.id
    
     claimItem = Claim.objects.values_list('status').filter(
-        date_claimed__gte = date_from,
-        date_claimed__lte = date_to,
+        validity_from__gte = date_from,
+        validity_to__lte = date_to,
         **dictGeo,
         status = 1
     ).count()
