6b9286fdbcfdd603b5016bdcefc2a198e1d26b1e
Tiah Kevel <k.choumene@cloudfacile.com>
2023-04-22 14:10:27 +0100

    Ajustement

--- reportcsu/report.py
+++ reportcsu/report.py
@@ -1,13 +1,13 @@
 from distutils.command import upload
 from reportcsu.models import invoice_csu_query
 
-from reportcsu.report_templates import rptInvoiceperPeriod
+from reportcsu.report_templates import rptInvoicePerFosa
 
 report_definitions = [ 
     {
         "name": "invoice_fosa_csu",
         "engine": 0,
-        "default_report":rptInvoiceperPeriod.template,
+        "default_report": rptInvoicePerFosa.template,
         "description": "Etat de paiement par fosa",
         "module": "reportcsu",
         "python_query": invoice_csu_query, 
